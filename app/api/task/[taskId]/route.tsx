d70f611bea81ebaee591133f5a21b6cfc8f5c923
MohammedSadok <mohammed.sadok.2003@gmail.com>
2023-12-11 18:21:02 +0100

    finish starter code

---
+++ app/api/task/[taskId]/route.tsx
@@ -0,0 +1,60 @@
+import { db } from "@/lib/db";
+import { routeContextSchema, taskPatchSchema } from "@/lib/validations/task";
+import * as z from "zod";
+
+export async function DELETE(
+  req: Request,
+  context: z.infer<typeof routeContextSchema>
+) {
+  try {
+    // Validate the route params.
+    const { params } = routeContextSchema.parse(context);
+    // Delete the task.
+    await db.task.delete({
+      where: {
+        id: params.taskId,
+      },
+    });
+
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify(error.issues), { status: 422 });
+    }
+    return new Response(null, { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  context: z.infer<typeof routeContextSchema>
+) {
+  try {
+    // Validate route params.
+    const { params } = routeContextSchema.parse(context);
+
+    // Get the request body and validate it.
+    const json = await req.json();
+    const body = taskPatchSchema.parse(json);
+
+    // Update the task.
+    await db.task.update({
+      where: {
+        id: params.taskId,
+      },
+      data: {
+        title: body?.title,
+        text: body?.text,
+        isCompleted: body?.isCompleted,
+      },
+    });
+
+    return new Response(null, { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify(error.issues), { status: 422 });
+    }
+
+    return new Response(null, { status: 500 });
+  }
+}
